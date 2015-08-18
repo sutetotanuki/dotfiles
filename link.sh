c85fd76d30f15a5d72a91f656e46453b2b0c5464
Nishida <nishida@zyyx.jp>
2015-08-18 14:53:00 +0900

    add gitignore_global

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
-
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
